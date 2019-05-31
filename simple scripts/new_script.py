c5cc28ea5403df40023489fcf8b205d3b52ff68b
Memohat <49382701+Memohat@users.noreply.github.com>
2019-05-31 10:36:17 -0400

    Added conditional for making ps1 file

--- simple scripts/new_script.py
+++ simple scripts/new_script.py
@@ -1,7 +1,9 @@
 #! python3
 # New script to make a file based on command line arguements
 # asks for file name if none is given in console.
-# makes python script with the name, also makes batch file for it.
+# makes python script with the name and .ps1 File
+
+ps1bool = False
 
 import os, sys
 
@@ -11,22 +13,23 @@ if len(sys.argv) > 1:
 else:
     name = input("Please enter the new file name: ")
 
-try:
+if not os.path.isfile(f'{name}.py'):
     python_file = open(f"{name}.py", "x")
     python_file.write('#! python3\n# Mehmet Hatip\n\n')
     python_file.close()
     print(f"File {name}.py successfully created")
-    os.startfile(f'{name}.py')
-except:
+else:
     print(f"Error, {name}.py file already exists")
-f"""{name}"""
-try:
-    os.chdir('ps1files')
-    with open(f"{name}.ps1", "x") as bat:
-        bat.write(
+os.startfile(f'{name}.py')
+
+if ps1bool:
+    try:
+        os.chdir('ps1files')
+        with open(f"{name}.ps1", "x") as bat:
+            bat.write(
 f"""cd ..
 py "{name}.py"
 cd ps1files""")
-    print(f"File {name}.ps1 successfully created.")
-except:
-    print(f"Error, {name}.ps1 file already exists")
+        print(f"File {name}.ps1 successfully created.")
+    except:
+        print(f"Error, {name}.ps1 file already exists")
